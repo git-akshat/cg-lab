4b41e92cdd46373166f393da0576efe0810e96e6
Akshat <31710005+git-akshat@users.noreply.github.com>
2019-02-04 21:39:13 +0530

    Update serpinski.c

--- Labset4/serpinski.c
+++ Labset4/serpinski.c
@@ -74,7 +74,7 @@ void myReshape(int w,int h)
 
 int main(int argc,char **argv)
 {
-    printf("Enter the number of divisions\n");
+    printf("Enter the number of divisions: ");
     scanf("%d",&n);
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
